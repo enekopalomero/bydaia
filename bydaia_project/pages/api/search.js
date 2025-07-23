8b54682856620da94f11c9c9d6a39192de1aee16
enekopalomero <palomero.eneko@gmail.com>
2025-07-23 10:52:09 +0200

    Delete bydaia_project directory

--- bydaia_project/pages/api/search.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json([
-    { price: "€399", stars: 3, rating: "7.8" },
-    { price: "€499", stars: 4, rating: "8.5" },
-    { price: "€599", stars: 5, rating: "9.1" },
-  ]);
-}
--- bydaia_project/utils/translations.js
+++
@@ -1,8 +0,0 @@
-export const translations = {
-  es: {
-    title: "Encuentra tu paquete de viaje ideal"
-  },
-  en: {
-    title: "Find your ideal travel package"
-  }
-};
